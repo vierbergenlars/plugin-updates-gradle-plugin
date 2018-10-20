3b7ffa88a20f9ac34b8fe8dd37152356602ceaf5
Lars Vierbergen <lars.vierbergen@xenit.eu>
2018-10-20 23:24:35 +0200

    Extract update checking logic to separate class

--- src/main/java/be/vbgn/gradle/pluginupdates/PluginUpdatesPlugin.java
+++ src/main/java/be/vbgn/gradle/pluginupdates/PluginUpdatesPlugin.java
@@ -1,19 +1,9 @@
 package be.vbgn.gradle.pluginupdates;
 
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.codehaus.groovy.runtime.MethodClosure;
 import org.gradle.BuildResult;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.artifacts.Configuration;
-import org.gradle.api.artifacts.Dependency;
-import org.gradle.api.artifacts.DependencySet;
-import org.gradle.api.artifacts.ExternalDependency;
-import org.gradle.api.artifacts.ResolvedConfiguration;
-import org.gradle.api.artifacts.ResolvedDependency;
 import org.gradle.api.initialization.Settings;
 import org.gradle.api.invocation.Gradle;
 import org.gradle.api.plugins.PluginAware;
@@ -47,12 +37,12 @@ public class PluginUpdatesPlugin implements Plugin<PluginAware> {
 
     private void onBuildFinished(BuildResult buildResult) {
         Gradle gradle = buildResult.getGradle();
-        gradle.getRootProject().getAllprojects()
-                .stream()
-                .flatMap(project2 -> checkUpdates(project2, project2.getBuildscript()
-                        .getConfigurations()
-                        .getAt("classpath"))
-                )
+        if (gradle.getStartParameter().isOffline()) {
+            gradle.getRootProject().getLogger()
+                    .info("Gradle is running in offline mode, plugins will not be checked for updates.");
+            return;
+        }
+        UpdateChecker.checkUpdates(gradle.getRootProject())
                 .filter(Update::isOutdated)
                 .forEach(update -> {
                     gradle.getRootProject().getLogger()
@@ -60,49 +50,6 @@ public class PluginUpdatesPlugin implements Plugin<PluginAware> {
                                     .getModuleGroup() + ":" + update.getModuleName() + " [" + update.getOldVersion()
                                     + " -> " + update.getNewVersion() + "]");
                 });
-        buildResult.rethrowFailure();
-
     }
-
-    private Stream<Update> checkUpdates(Project project, Configuration configuration) {
-        ResolvedConfiguration oldConfiguration = configuration.getResolvedConfiguration();
-        ResolvedConfiguration upToDateConfiguration = createLatestConfiguration(project, configuration)
-                .getResolvedConfiguration();
-
-        Set<ResolvedDependency> upToDateDependencies = upToDateConfiguration.getFirstLevelModuleDependencies();
-
-        return oldConfiguration.getFirstLevelModuleDependencies().stream()
-                .map(oldResolvedDependency -> new Update(project, configuration, oldResolvedDependency,
-                        findDependency(upToDateDependencies, oldResolvedDependency)
-                                .orElseGet(() -> oldResolvedDependency)));
-    }
-
-    private Optional<ResolvedDependency> findDependency(Set<ResolvedDependency> dependencies,
-            ResolvedDependency dependency) {
-        return dependencies.stream()
-                .filter(dependency2 -> dependency.getModuleGroup().equals(dependency2.getModuleGroup()))
-                .filter(dependency1 -> dependency.getModuleName().equals(dependency1.getModuleName()))
-                .findFirst();
-    }
-
-    private Configuration createLatestConfiguration(Project project, Configuration configuration) {
-        DependencySet dependencies = configuration.getDependencies();
-
-        Stream<Dependency> newDependenciesStream = dependencies.stream()
-                .filter(dependency -> dependency instanceof ExternalDependency)
-                .map(dependency -> createLatestDependency(project, dependency));
-
-        Configuration newConfiguration = configuration.copy();
-        newConfiguration.setTransitive(false);
-        newConfiguration.getDependencies().clear();
-
-        newConfiguration.getDependencies().addAll(newDependenciesStream.collect(Collectors.toSet()));
-        return newConfiguration;
-    }
-
-    private Dependency createLatestDependency(Project project, Dependency dependency) {
-        return project.getDependencies().create(dependency.getGroup() + ":" + dependency.getName() + ":+");
-    }
-
 
 }
---
+++ src/main/java/be/vbgn/gradle/pluginupdates/UpdateChecker.java
@@ -0,0 +1,69 @@
+package be.vbgn.gradle.pluginupdates;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.artifacts.DependencySet;
+import org.gradle.api.artifacts.ExternalDependency;
+import org.gradle.api.artifacts.ResolvedConfiguration;
+import org.gradle.api.artifacts.ResolvedDependency;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
+
+public class UpdateChecker {
+
+    public static Logger LOGGER = Logging.getLogger(UpdateChecker.class);
+
+    public static Stream<Update> checkUpdates(Project project) {
+        return project.getAllprojects()
+                .parallelStream()
+                .flatMap(project1 -> checkUpdates(project1,
+                        project1.getBuildscript().getConfigurations().getAt("classpath")));
+    }
+
+    public static Stream<Update> checkUpdates(Project project, Configuration configuration) {
+        LOGGER.debug("Checking updates for project {}, configuration {}", project, configuration);
+        ResolvedConfiguration oldConfiguration = configuration.getResolvedConfiguration();
+        ResolvedConfiguration upToDateConfiguration = createLatestConfiguration(project, configuration)
+                .getResolvedConfiguration();
+
+        Set<ResolvedDependency> upToDateDependencies = upToDateConfiguration.getFirstLevelModuleDependencies();
+
+        return oldConfiguration.getFirstLevelModuleDependencies().parallelStream()
+                .map(oldResolvedDependency -> new Update(project, configuration, oldResolvedDependency,
+                        findDependency(upToDateDependencies, oldResolvedDependency)
+                                .orElseGet(() -> oldResolvedDependency)));
+    }
+
+    private static Optional<ResolvedDependency> findDependency(Set<ResolvedDependency> dependencies,
+            ResolvedDependency dependency) {
+        return dependencies.parallelStream()
+                .filter(dependency2 -> dependency.getModuleGroup().equals(dependency2.getModuleGroup()))
+                .filter(dependency1 -> dependency.getModuleName().equals(dependency1.getModuleName()))
+                .findFirst();
+    }
+
+    private static Configuration createLatestConfiguration(Project project, Configuration configuration) {
+        DependencySet dependencies = configuration.getDependencies();
+
+        Stream<Dependency> newDependenciesStream = dependencies.parallelStream()
+                .filter(dependency -> dependency instanceof ExternalDependency)
+                .map(dependency -> createLatestDependency(project, dependency));
+
+        Configuration newConfiguration = configuration.copy();
+        newConfiguration.setTransitive(false);
+        newConfiguration.getDependencies().clear();
+
+        newConfiguration.getDependencies().addAll(newDependenciesStream.collect(Collectors.toSet()));
+        return newConfiguration;
+    }
+
+    private static Dependency createLatestDependency(Project project, Dependency dependency) {
+        return project.getDependencies().create(dependency.getGroup() + ":" + dependency.getName() + ":+");
+    }
+
+}
