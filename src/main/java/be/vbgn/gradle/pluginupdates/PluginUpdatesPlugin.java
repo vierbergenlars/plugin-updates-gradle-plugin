a804df87b65e3edd4b038e9bf66be63adb217bfd
Lars Vierbergen <vierbergenlars@gmail.com>
2019-01-12 13:12:12 +0100

    Move update checking logic from the plugin to an other class

--- src/main/java/be/vbgn/gradle/pluginupdates/PluginUpdatesPlugin.java
+++ src/main/java/be/vbgn/gradle/pluginupdates/PluginUpdatesPlugin.java
@@ -1,20 +1,12 @@
 package be.vbgn.gradle.pluginupdates;
 
-import be.vbgn.gradle.pluginupdates.dsl.internal.UpdateBuilder;
-import be.vbgn.gradle.pluginupdates.dsl.internal.UpdateCheckerBuilderConfiguration;
 import be.vbgn.gradle.pluginupdates.internal.ConfigurationCollector;
 import be.vbgn.gradle.pluginupdates.internal.StreamUtil;
+import be.vbgn.gradle.pluginupdates.internal.UpdateChecker;
 import be.vbgn.gradle.pluginupdates.update.Update;
-import be.vbgn.gradle.pluginupdates.update.checker.DefaultUpdateChecker;
-import be.vbgn.gradle.pluginupdates.update.finder.DefaultUpdateFinder;
-import be.vbgn.gradle.pluginupdates.update.finder.DefaultVersionProvider;
-import be.vbgn.gradle.pluginupdates.update.finder.UpdateFinder;
-import be.vbgn.gradle.pluginupdates.update.finder.VersionProvider;
-import be.vbgn.gradle.pluginupdates.update.finder.internal.InvalidResolvesCache;
 import be.vbgn.gradle.pluginupdates.update.formatter.DefaultUpdateFormatter;
 import be.vbgn.gradle.pluginupdates.update.formatter.PluginUpdateFormatter;
 import be.vbgn.gradle.pluginupdates.update.formatter.UpdateFormatter;
-import java.util.LinkedList;
 import java.util.List;
 import javax.annotation.Nonnull;
 import org.codehaus.groovy.runtime.MethodClosure;
@@ -22,19 +14,17 @@ import org.gradle.BuildResult;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.initialization.Settings;
-import org.gradle.api.internal.GradleInternal;
 import org.gradle.api.invocation.Gradle;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.logging.Logging;
 import org.gradle.api.plugins.PluginAware;
-import org.gradle.cache.CacheRepository;
 import org.gradle.util.GradleVersion;
 
 public class PluginUpdatesPlugin implements Plugin<PluginAware> {
 
     public static String PLUGIN_ID = "be.vbgn.plugin-updates";
     private static Logger LOGGER = Logging.getLogger(PluginUpdatesPlugin.class);
-    private ConfigurationCollector configurationCollector;
+    private UpdateChecker updateChecker;
 
     @Override
     public void apply(@Nonnull PluginAware thing) {
@@ -51,6 +41,7 @@ public class PluginUpdatesPlugin implements Plugin<PluginAware> {
         }
     }
 
+
     private boolean isOnline(@Nonnull Gradle gradle) {
         if (gradle.getStartParameter().isOffline()) {
             LOGGER.info("Gradle is running in offline mode, plugins will not be checked for updates");
@@ -63,7 +54,7 @@ public class PluginUpdatesPlugin implements Plugin<PluginAware> {
         Gradle gradle = project.getGradle();
         if (isOnline(gradle)) {
             LOGGER.debug("Register buildFinished callback for single project");
-            configurationCollector = new ConfigurationCollector(gradle);
+            configurePlugin(gradle);
             project.getGradle().buildFinished(new MethodClosure(this, "onBuildFinished").curry(project));
         }
     }
@@ -72,7 +63,7 @@ public class PluginUpdatesPlugin implements Plugin<PluginAware> {
         Gradle gradle = settings.getGradle();
         if (isOnline(gradle)) {
             LOGGER.debug("Register buildFinished callback");
-            configurationCollector = new ConfigurationCollector(gradle);
+            configurePlugin(gradle);
             gradle.buildFinished(new MethodClosure(this, "onBuildFinished"));
         }
     }
@@ -86,7 +77,7 @@ public class PluginUpdatesPlugin implements Plugin<PluginAware> {
         }
         if (isOnline(gradle)) {
             LOGGER.debug("Register buildFinished callback");
-            configurationCollector = new ConfigurationCollector(gradle);
+            configurePlugin(gradle);
             gradle.buildFinished(new MethodClosure(this, "onBuildFinished"));
         }
     }
@@ -109,46 +100,23 @@ public class PluginUpdatesPlugin implements Plugin<PluginAware> {
         runBuildscriptUpdateCheck(project);
     }
 
+    private void configurePlugin(@Nonnull Gradle gradle) {
+        updateChecker = new UpdateChecker(gradle, new ConfigurationCollector(gradle));
+    }
+
+
     private void runBuildscriptUpdateCheck(@Nonnull Project project) {
         try {
-            UpdateCheckerBuilderConfiguration checkerConfiguration = configurationCollector.forProject(project);
+            List<Update> updates = updateChecker
+                    .getUpdates(project, project.getBuildscript().getConfigurations().getAt("classpath"));
+
             UpdateFormatter updateFormatter = new PluginUpdateFormatter(new DefaultUpdateFormatter());
 
-            UpdateBuilder updateBuilder = checkerConfiguration.getUpdateBuilder();
-
-            CacheRepository cacheRepository = ((GradleInternal) project.getGradle()).getServices()
-                    .get(CacheRepository.class);
-            InvalidResolvesCache invalidResolvesCache = null;
-            try {
-                invalidResolvesCache = new InvalidResolvesCache(cacheRepository);
-            } catch (NoClassDefFoundError e) {
-                LOGGER.warn(
-                        "Some required gradle classes are missing. Invalid resolves cache is disabled, which will slow down plugin update checks.");
-                LOGGER.debug("Full exception for above warning", e);
-            }
-
-            VersionProvider versionProvider = updateBuilder.buildVersionProvider(new DefaultVersionProvider());
-            DefaultUpdateFinder defaultUpdateFinder = new DefaultUpdateFinder(project.getBuildscript(),
-                    versionProvider);
-            if (invalidResolvesCache != null) {
-                defaultUpdateFinder.setInvalidResolvesCache(invalidResolvesCache);
-            }
-            UpdateFinder updateFinder = updateBuilder.buildUpdateFinder(defaultUpdateFinder);
-
-            DefaultUpdateChecker updateChecker = new DefaultUpdateChecker(updateFinder);
-
-            List<Update> updates = new LinkedList<>();
-
-            updateChecker.getUpdates(project.getBuildscript().getConfigurations().getAt("classpath"))
-                    .filter(Update::isOutdated)
-                    .forEach(updates::add);
-
-            if(invalidResolvesCache != null) {
-                invalidResolvesCache.close();
-            }
             updates.forEach(update -> {
-                LOGGER.warn("Plugin is outdated in " + project.toString() + ": " + updateFormatter
-                        .format(update));
+                if (update.isOutdated()) {
+                    LOGGER.warn("Plugin is outdated in " + project.toString() + ": " + updateFormatter
+                            .format(update));
+                }
             });
         } catch (Throwable e) {
             LOGGER.error("Plugin update check failed.", e);
---
+++ src/main/java/be/vbgn/gradle/pluginupdates/internal/UpdateChecker.java
@@ -0,0 +1,86 @@
+package be.vbgn.gradle.pluginupdates.internal;
+
+import be.vbgn.gradle.pluginupdates.dsl.internal.UpdateBuilder;
+import be.vbgn.gradle.pluginupdates.dsl.internal.UpdateCheckerBuilderConfiguration;
+import be.vbgn.gradle.pluginupdates.update.Update;
+import be.vbgn.gradle.pluginupdates.update.checker.DefaultUpdateChecker;
+import be.vbgn.gradle.pluginupdates.update.finder.DefaultUpdateFinder;
+import be.vbgn.gradle.pluginupdates.update.finder.DefaultVersionProvider;
+import be.vbgn.gradle.pluginupdates.update.finder.UpdateFinder;
+import be.vbgn.gradle.pluginupdates.update.finder.VersionProvider;
+import be.vbgn.gradle.pluginupdates.update.finder.internal.InvalidResolvesCache;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import javax.annotation.Nullable;
+import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.internal.GradleInternal;
+import org.gradle.api.invocation.Gradle;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
+import org.gradle.cache.CacheRepository;
+
+public class UpdateChecker {
+    private static Logger LOGGER = Logging.getLogger(UpdateChecker.class);
+    private Gradle gradle;
+    private ConfigurationCollector configurationCollector;
+    @Nullable
+    private Optional<InvalidResolvesCache> invalidResolvesCache = null;
+
+
+    public UpdateChecker(Gradle gradle, ConfigurationCollector configurationCollector) {
+        this.gradle = gradle;
+        this.configurationCollector = configurationCollector;
+    }
+
+    public List<Update> getUpdates(Project project, Configuration configuration) {
+        UpdateCheckerBuilderConfiguration updateCheckerBuilderConfiguration = configurationCollector.forProject(project);
+        UpdateBuilder updateBuilder = updateCheckerBuilderConfiguration.getUpdateBuilder();
+        VersionProvider versionProvider = updateBuilder.buildVersionProvider(new DefaultVersionProvider());
+        DefaultUpdateFinder defaultUpdateFinder = new DefaultUpdateFinder(project.getBuildscript(),
+                versionProvider);
+        UpdateFinder updateFinder = updateBuilder.buildUpdateFinder(defaultUpdateFinder);
+
+        DefaultUpdateChecker updateChecker = new DefaultUpdateChecker(updateFinder);
+
+        getInvalidResolvesCache()
+                .ifPresent(defaultUpdateFinder::setInvalidResolvesCache);
+
+        List<Update> updates = updateChecker.getUpdates(configuration).collect(Collectors.toList());
+
+        getInvalidResolvesCache()
+                .ifPresent(invalidResolvesCache -> invalidResolvesCache.close());
+
+        return updates;
+
+    }
+
+    private static boolean classExists(String className) {
+        try {
+            Class.forName(className);
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+    }
+
+    private Optional<InvalidResolvesCache> getInvalidResolvesCache() {
+        if(invalidResolvesCache == null) {
+            CacheRepository cacheRepository = ((GradleInternal) gradle).getServices()
+                    .get(CacheRepository.class);
+            invalidResolvesCache = Optional.empty();
+            try {
+                if(classExists("org.gradle.cache.LockOptions")) {
+                    invalidResolvesCache = Optional.of(new InvalidResolvesCache(cacheRepository));
+                }
+            } catch (NoClassDefFoundError e) {
+                LOGGER.warn(
+                        "Some required gradle classes are missing. Invalid resolves cache is disabled, which will slow down plugin update checks.");
+                LOGGER.debug("Full exception for above warning", e);
+            }
+        }
+        return invalidResolvesCache;
+    }
+
+}
